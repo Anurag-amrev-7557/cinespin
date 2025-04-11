f14b983b2fc84a8b2e476b755def1d8aa8d8bdd9
Anurag-amrev7557 <mindreaders7557@gmail.com>
2025-04-11 22:21:49 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { imagetools } from 'vite-imagetools';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(), imagetools()],
+    plugins: [react(), imagetools(),     
+      viteCompression({
+      algorithm: 'brotliCompress', // or 'brotliCompress'
+      ext: '.br',        // output extension
+      deleteOriginFile: false, // keeps original .js file
+    })],
     base: '/', // ✅ Ensures correct asset paths for production on Render
     optimizeDeps: {
       include: ['react-slick'],
